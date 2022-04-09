ded0c3031ae1923264b19eaee992e8a83d0c874f
Kevin Soto Guerrero <desarrollo7@aveonline.co>
2022-04-09 16:04:42 -0500

    feat: validate dom, properties document, add event listeners

--- dom/document.js
+++ dom/document.js
@@ -1,5 +1,11 @@
 var lienzo = document.getElementById("dibujoCanvas").getContext("2d");
 var lienzo2 = document.getElementById("dibujoCanvas2").getContext("2d");
+var buttonEnviar =document.getElementById("buttonEnviar");
+
+var anchoLienzo2 = document.getElementById("dibujoCanvas2").width;
+
+//poner el escuchador de eventos, el evento es el click y la funcion que llama es dibujarCanvas2
+buttonEnviar.addEventListener("click", dibujarCanvas2);
 
 makeLine(lienzo,"blue", 50, 0, 50, 300);
 makeLine(lienzo,"blue", 50, 150, 150, 0);
@@ -9,11 +15,21 @@ makeLine(lienzo,"red", 0, 0, 200, 0);
 makeLine(lienzo,"red", 200, 300, 0, 300);
 makeLine(lienzo,"red", 200, 300, 200, 0);
 
-for(var i=0; i<=300; i+=15 ){
-    makeLine(lienzo2, "green", 0, i, i,300)
-    makeLine(lienzo2, "green", i, 0, 300,i)
-    makeLine(lienzo2, "green", 300, i, 300-i,300)
-    makeLine(lienzo2, "green", 0, i, 300-i,0)
+function dibujarCanvas2(){
+  cantidadLineas=document.getElementById("inputLineas").value
+  
+  //el valor que trae del document es tipo string por lo cual el +1 queda como una concatenacion por eso hay que hacer el parseInt
+  console.log(cantidadLineas+1)
+
+  pxPorLinea=parseInt(anchoLienzo2/cantidadLineas)
+  console.log(pxPorLinea+1)
+  
+  for(var i=0; i<=anchoLienzo2; i+=pxPorLinea ){
+      makeLine(lienzo2, "green", 0, i, i,anchoLienzo2)
+      makeLine(lienzo2, "green", i, 0, anchoLienzo2,i)
+      makeLine(lienzo2, "green", anchoLienzo2, i, anchoLienzo2-i,anchoLienzo2)
+      makeLine(lienzo2, "green", 0, i, anchoLienzo2-i,0)
+  }
 }
 
 function makeLine(lienzo, color, xi, yi, xf, yf) {
